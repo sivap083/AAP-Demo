046672f6c31e98b2fa15641666658647e9d5bc9a
Mohini Rahate <mohini.rahate@perficient.com>
2023-11-06 17:55:17 +0530

    update

--- inventory-scripts/inventory-script.py
+++ inventory-scripts/inventory-script.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
 import json
 
-# Read the JSON data from the file
-with open('json_payload.json', 'r') as json_file:
-    inventory_data = json.load(json_file)
+def read_inventory_from_file(file_path):
+    with open(file_path, 'r') as json_file:
+        return json.load(json_file)
 
-# Print the JSON data
-print(json.dumps(inventory_data))
+if __name__ == "__main__":
+    inventory_data = read_inventory_from_file('./inventory-scripts/json_payload.json')
+    print(json.dumps(inventory_data, indent=4))
